29f2e224df1739b73a8ee4a902b322718cc88453
DanielRGilles <danryangilles@gmail.com>
2021-10-08 11:43:47 -0700

    finished writing tests for util functions

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
-
-// const { execSync } = require('child_process');
-
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
-// const client = require('../lib/client');
+
 
 describe('app routes', () => {
   describe('routes', () => {
@@ -29,46 +26,39 @@ describe('app routes', () => {
 
       const expectation = [
         {
-          'forecast': 'Partly cloudy until afternoon.',
-          'time': 'Tuesday, June 29, 2021'
+          'forecast': expect.any(String),
+          'time': expect.any(String)
         },
         {
-          'forecast':'Mostly cloudy in the morning.',
-          'time': 'Wednesday, June 30, 2021'
+          'forecast':expect.any(String),
+          'time': expect.any(String)
         }];
     
 
       const data = await fakeRequest(app)
-        .get('/weather')
+        .get('/weather?latitude=39&longitude=-78')
         .expect('Content-Type', /json/)
         .expect(200);
 
-      expect(data.body).toEqual(expectation);
+      expect(data.body).toEqual(expect.arrayContaining(expectation));
     });
     test('returns reviews', async() => {
 
-      const expectation = [
+      const expectation = 
         {
-          'name': 'Pike Place Chowder',
-          'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/ijju-wYoRAxWjHPTCxyQGQ/o.jpg',
-          'price': '$$   ',
-          'rating': '4.5',
-          'url': 'https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA'
-        },
-        {
-          'name': 'Umi Sake House',
-          'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/c-XwgpadB530bjPUAL7oFw/o.jpg',
-          'price': '$$   ',
-          'rating': '4.0',
-          'url': 'https://www.yelp.com/biz/umi-sake-house-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA'
-        }];
+          'name': expect.any(String),
+          'image_url': expect.any(String),
+          'price': expect.any(String),
+          'rating': expect.any(Number),
+          'url': expect.any(String),
+        };
 
       const data = await fakeRequest(app)
-        .get('/reviews')
+        .get('/reviews?latitude=38&longitude=-78')
         .expect('Content-Type', /json/)
         .expect(200);
 
-      expect(data.body).toEqual(expectation);
+      expect(data.body[0]).toEqual(expectation);
     });
   });
 });
